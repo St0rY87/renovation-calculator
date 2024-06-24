20669be3aad0267763d83a18c66f4f0d7555ce65
Dmitriy <mr.dima337@gmail.com>
2024-06-24 16:48:05 +0300

    JS folder  HandleInput.js Implement hnadle inputs

---
+++ src/js/helpers/handleinput.js
@@ -0,0 +1,19 @@
+const handleinput = () => {
+    const inputs = document.querySelectorAll('.calculator__item-input');
+
+    inputs.forEach(input => {
+        input.addEventListener('input', (e) => {
+            let value = e.target.value;
+            value = value.replace(/\D/g, '');
+            if (value > 3) {
+                value = value.slice(0, 3)
+            }
+            e.target.value = value;
+        })
+    })
+
+}
+
+
+
+export default handleinput;
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+import handleinput from "./helpers/handleinput"
+
+window.addEventListener('DOMContentLoaded', () => {
+    handleinput();
+
+})
