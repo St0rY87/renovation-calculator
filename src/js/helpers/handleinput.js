597ac169b1f3efdb0d915fd083554e0f15958ca6
Dmitriy <mr.dima337@gmail.com>
2024-06-26 22:27:25 +0300

    JS file HandleInput.js Add code for handling negative numbers

--- src/js/helpers/handleinput.js
+++ src/js/helpers/handleinput.js
@@ -4,16 +4,24 @@ const handleInput = () => {
     inputs.forEach(input => {
         input.addEventListener('input', (e) => {
             let value = e.target.value;
-            value = value.replace(/\D/g, '');
-            if (value > 3) {
-                value = value.slice(0, 3)
+            value = value.replace(/[^0-9-]/g, '');
+
+            if (value.length > 3) {
+                value = value.slice(0, 3);
+            };
+
+            if (value.indexOf('-') !== -1) {
+                const parts = value.split('-');
+                value = '-' + parts.join('');
+                e.target.classList.add('error-input');
+                console.log(e.target);
             }
+            else e.target.classList.remove('error-input');
+
             e.target.value = value;
         })
     })
-
 }
 
 
-
-export default handleInput;
+export default handleInput
