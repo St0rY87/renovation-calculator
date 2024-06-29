ca5c6e98b646840af079f239a2fe9e44e3213c35
Dmitriy <mr.dima337@gmail.com>
2024-06-29 13:04:23 +0300

    JS ShowData.js Implement new helper for displaying data from DB

---
+++ src/js/helpers/showData.js
@@ -0,0 +1,25 @@
+import { getResource } from '../services/services';
+const showData = () => {
+    getResource('http://localhost:3000/squares')
+        .then(res => {
+            const listItems = document.querySelector('.calculator__content_square');
+            res.forEach(item => {
+                const { name, value, id } = item;
+                const li = document.createElement('LI');
+                li.classList.add('calculator__item');
+                li.id = id;
+                li.innerHTML = `<label class="calculator__item-text"><span>${name}:</span>
+                        <div class="calculator__item-inner">
+                            <input class="calculator__item-input" type="text" placeholder="0.0">
+                            <span class="calculator__item-symb">м2</span>
+                        </div>
+                    </label>`;
+
+                listItems.appendChild(li);
+                document.querySelectorAll('.calculator__item-input').forEach(item => item.value = value);
+            })
+
+        })
+}
+
+export default showData
