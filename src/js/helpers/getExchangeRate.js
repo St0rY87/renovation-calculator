41746b192315698b1d7c45e77bb6da89e80c880a
Dmitriy <mr.dima337@gmail.com>
2024-07-11 17:53:55 +0300

    JS getExchangeRate.js Refactore code

--- src/js/helpers/getExchangeRate.js
+++ src/js/helpers/getExchangeRate.js
@@ -3,7 +3,8 @@ import { getResource } from "../services/services";
 const getExchangeRate = (state, url) => {
     getResource(url)
         .then(data => {
-            state.exchangeRate = Number(data[0].buy)
+            state.exchangeRate = Number(data[0].buy) || 0;
+            console.log(state)
         })
 }
 export default getExchangeRate;
